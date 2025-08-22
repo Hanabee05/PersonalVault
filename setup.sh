3d81e6c2f93f822185b0b8e79910a336f129f1b5
Hanabee05 <witankatyes@gmail.com>
2025-08-22 05:44:46 +0000

    Frist commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+PROJECT_NAME="PersonalVault"
+
+mkdir -p $PROJECT_NAME/src
+mkdir -p $PROJECT_NAME/test
+
+touch $PROJECT_NAME/src/vault.py
+touch $PROJECT_NAME/src/cli.py
+touch $PROJECT_NAME/src/utils.py
+
+echo "YOSHH"
